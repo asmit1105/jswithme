42a9dc339996e90e9350d30ff3a73b73c7339ff2
asmit1105 <asmitsharma1105@gmail.com>
2026-03-17 17:33:53 +0000

    added comment

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -2,6 +2,6 @@ const userName = "_asmitsharma_"
 const userId = 441327866
 let password = "011005"
 var userCity = "Mumbai"
-
+// Prefer not to use var because of issue in block scope and functional scope
 console.log(userId)
 console.table([userName, password, userCity])
