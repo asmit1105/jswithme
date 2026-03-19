f4313e60dcb460a5aa6a102762b8f9214a9ba684
asmit1105 <asmitsharma1105@gmail.com>
2026-03-19 18:59:22 +0000

    conversion operations

---
+++ 01_basics/03_conversions.js
@@ -0,0 +1,24 @@
+let price = 21;
+
+// Converted number to number
+let convertedPrice = Number(price);
+//console.log(typeof convertedPrice); 
+
+// "33" => 33
+// "33abc" => NaN
+//  true => 1; false => 0
+
+
+// Converted number to string
+let stringPrice = String(price);
+//console.log(typeof stringPrice);
+
+// Converted number to boolean
+let isLoggedIn = "asmit";
+let userStatus = Boolean(isLoggedIn);
+console.log(userStatus);
+console.log(typeof userStatus);
+
+// 1 => true; 0 => false
+// "" => false
+// "asmit" => true
