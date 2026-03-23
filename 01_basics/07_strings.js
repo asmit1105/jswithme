359c2ca6fc36206a5192f1b9833256d85e311aee
Asmit Sharma <asmitsharma1105@gmail.com>
2026-03-23 18:49:24 +0000

    strings

---
+++ 01_basics/07_strings.js
@@ -0,0 +1,43 @@
+// STRINGS
+
+let name = "Asmit";
+let followers = 49500;
+
+console.log(`My name is ${name} and I have ${followers} followers`);
+
+// STRING FUNCTIONS
+
+// Length
+console.log(name.length) // 5
+
+// charat
+console.log(name.charAt(3)); // i
+
+// at
+console.log(name.at(-1)); // t
+
+// charCodeAt
+
+console.log(name.charCodeAt(3)); //UTF-16
+
+//slice
+
+let lang = "Javascript";
+console.log(lang.slice(0,4)); // Java
+
+// substring
+
+console.log(lang.substring(1,5)); // avas
+
+// upper/lowercase
+
+let gameName = "asmit1105";
+console.log(gameName.toUpperCase()); //ASMIT1105
+
+// Combining - concat
+
+console.log(name.concat(" ", gameName)); // Asmit asmit1105 (not a very good example tbh)
+
+//Remove whitespace - trim
+
+
