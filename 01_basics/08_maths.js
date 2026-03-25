32b8972caca9fe42e7ed897d0b404001cafd45e9
Asmit Sharma <asmitsharma1105@gmail.com>
2026-03-25 10:20:55 +0000

    till numbers

--- 01_basics/08_maths.js
+++ 01_basics/08_maths.js
@@ -1,2 +1,38 @@
 //Maths
-console.log("Asmit");
+const score = 50; // Primitive Number (Normal)
+console.log(score);
+console.log(typeof score);
+
+const ammos = new Number(78); // Number Object
+console.log(ammos);
+console.log(typeof ammos);
+
+// +++++++++ toString +++++++++++++++
+
+const age = 18;
+console.log(typeof age.toString()); //changed to string
+
+// +++++++++ toFixed (Rounds of to specific decimal places)
+
+const pi = 3.14159;
+console.log(pi.toFixed(2)); // 3.14
+
+// toPrecision
+
+const maxHealth = 99.9984
+console.log(maxHealth.toPrecision(3)); //100
+
+// Number System Conversion
+const hundreds = 1000000;
+console.log(hundreds); //1000000
+console.log(hundreds.toLocaleString('en-US')); //1,000,000 (International Num System)
+console.log(hundreds.toLocaleString('en-IN')); // 10,00,000 (Indian Number System)
+
+/* --------------MATHS---------------- */
+
+
+
+
+
+
+
