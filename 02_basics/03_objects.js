87aece4abdeff1bb889ed09f802af3e3c20779ba
Asmit Sharma <asmitsharma1105@gmail.com>
2026-04-02 18:44:19 +0000

    1st commit

--- 02_basics/03_objects.js
+++ 02_basics/03_objects.js
@@ -12,3 +12,14 @@ console.log(obj);
 obj.name = "Asmit Sharma";
 
 console.log(obj);
+
+const gameDev = {
+  name: "Asmit",
+  skills: ["C++", "Unity", "Blender"],
+  isLearning: true,
+  uploadVideo: function() {
+    console.log("Uploading devlog...");
+  }
+};
+
+
