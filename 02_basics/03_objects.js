eb26c94a2bc29587836d4a57e6ff9a6c2e4e220c
Asmit Sharma <asmitsharma1105@gmail.com>
2026-04-03 19:40:09 +0000

    gg

--- 02_basics/03_objects.js
+++ 02_basics/03_objects.js
@@ -1,33 +1,38 @@
-const object = {
-    name : "Asmit",
-    age : 20,
+// singleton
+// Object.create
+
+// object literals
+
+const mySym = Symbol("key1")
+
+
+const JsUser = {
+    name: "Hitesh",
+    "full name": "Hitesh Choudhary",
+    [mySym]: "mykey1",
+    age: 18,
+    location: "Jaipur",
+    email: "hitesh@google.com",
+    isLoggedIn: false,
+    lastLoginDays: ["Monday", "Saturday"]
 }
 
-console.log(object);
+// console.log(JsUser.email)
+// console.log(JsUser["email"])
+// console.log(JsUser["full name"])
+// console.log(JsUser[mySym])
 
-const obj = new Object({ name: "Asmit"});
+JsUser.email = "hitesh@chatgpt.com"
+// Object.freeze(JsUser)
+JsUser.email = "hitesh@microsoft.com"
+// console.log(JsUser);
 
-console.log(obj);
+JsUser.greeting = function(){
+    console.log("Hello JS user");
+}
+JsUser.greetingTwo = function(){
+    console.log(`Hello JS user, ${this.name}`);
+}
 
-obj.name = "Asmit Sharma";
-const user = {
-  name: "Asmit",
-  greet: function() {
-    console.log("Hello " + this.name);
-  }
-};
-
-user.greet(); // Hello Asmit
-
-console.log(obj);
-
-const gameDev = {
-  name: "Asmit",
-  skills: ["C++", "Unity", "Blender"],
-  isLearning: true,
-  uploadVideo: function() {
-    console.log("Uploading devlog...");
-  }
-};
-
-
+console.log(JsUser.greeting());
+console.log(JsUser.greetingTwo());
