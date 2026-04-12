0a72747cf6c951c027a93dfbb0ddd8fc72c96780
Asmit Sharma <asmitsharma1105@gmail.com>
2026-04-12 19:44:36 +0000

    doneee

--- 02_basics/04_objectcont.js
+++ 02_basics/04_objectcont.js
@@ -5,3 +5,28 @@ hypeXuser.name = "Govind";
 hypeXuser.isLoggedIn = false;
 
 console.log(hypeXuser);
+
+const randomuser = {
+    fullName: {
+        firstname: "asmit", 
+            lastname: "sharma"
+        }
+    }
+
+    console.log(randomuser.fullName.firstname);
+    
+
+    const obj1 = {1:"a" , 2:"b"}
+     const obj2 = {5:"v" , 0:"b" , 4:"n"};
+
+     const obj3 = {...obj1, ...obj2};
+     console.log(obj3);
+
+     console.log(Object.keys(hypeXuser));
+        console.log(Object.values(hypeXuser));
+        console.log(Object.entries(hypeXuser));
+
+            console.log(hypeXuser.hasOwnProperty("isLoggedIn"));
+        
+     
+
