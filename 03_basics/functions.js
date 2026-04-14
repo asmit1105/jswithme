ff3269328859a6141bbe86f77f27f32cb8686032
Asmit Sharma <asmitsharma1105@gmail.com>
2026-04-14 17:38:33 +0000

    funcdfday

--- 03_basics/functions.js
+++ 03_basics/functions.js
@@ -10,3 +10,31 @@ function myName() {
 }
 
 myName();
+
+// function addTwoNumbers(number1, number2){
+
+//     console.log(number1 + number2);
+// }
+
+function addTwoNumbers(number1, number2){
+
+    // let result = number1 + number2
+    // return result
+    return number1 + number2
+}
+
+const result = addTwoNumbers(3, 5)
+
+// console.log("Result: ", result);
+
+
+function loginUserMessage(username = "sam"){
+    if(!username){
+        console.log("PLease enter a username");
+        return
+    }
+    return `${username} just logged in`
+}
+
+// console.log(loginUserMessage("hitesh"))
+// console.log(loginUserMessage("hitesh"))
